3bff691e3cc939bfc29bd088b5ca42b178247e84
Sylvain Dedieu <sylvaindedieu@Sylvains-MacBook-Pro.local>
2023-01-24 13:20:26 +0100

    Add cache for lotr characters

--- src/app/services/lotr-character.service.ts
+++ src/app/services/lotr-character.service.ts
@@ -1,15 +1,16 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { EMPTY, shareReplay, map, Observable, tap } from 'rxjs';
+import { EMPTY, shareReplay, map, Observable, tap, combineLatestWith, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Character, CharacterRaceFilter, CharacterRealm, Characters } from '../interfaces/character';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LotrCharacterService {
   
-  constructor(private _http: HttpClient){}
+  constructor(private _http: HttpClient, private _localStorageService: LocalStorageService){}
   
   private _cachedCharacters$!: Observable<Characters>;
 
@@ -18,11 +19,15 @@ export class LotrCharacterService {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${environment.secretToken}`
     })
-    if(!this._cachedCharacters$) {
-     this._cachedCharacters$ = this._http.get<{docs: Characters}>('https://the-one-api.dev/v2/character', {headers}).pipe(
-        map(res => res.docs), 
-        shareReplay(1)
-      )
+    if (!this._cachedCharacters$) {
+      const stored = this._localStorageService.get<Characters>('characters');
+      this._cachedCharacters$ = (stored
+        ? of(stored)
+        : this._http.get<{ docs: Characters }>('https://the-one-api.dev/v2/character', { headers }).pipe(
+          map(res => res.docs),
+          tap(characters => this._localStorageService.set('characters', characters))
+        )
+      ).pipe(shareReplay(1))
     }
     return this._cachedCharacters$;
   }
